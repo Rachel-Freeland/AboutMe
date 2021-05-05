d04f51a23d47894ed799faf2b9634bf521d7099c
Rachel Freeland <rachel.freeland2@gmail.com>
2021-05-05 06:05:41 -0500

    fixed syntax errors

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ console.log('Hello World!');
 // ask user their name so they can receive a custom greeting
 let userName = prompt('Hi user! What is your name?');
 // console.log('Greetings ' + userName +'! Welcome to my page!');
-alert('Greetings ' + userName +'! Welcome to my page!')
+alert('Greetings ' + userName +'! Welcome to my page!');
 
 // Ask a question that expects a 'yes' or 'no' answer. Question 1
 let dogAnswer = prompt('Do you think Rachel has a dog? Please answer Yes or No');
@@ -17,9 +17,9 @@ let lowercaseDog = dogAnswer.toLowerCase();
 // determines acceptable answers and returns an alert to whether the user was right or wrong
 if (lowercaseDog === 'yes' || lowercaseDog === 'y') {
   // console.log('You are correct, ' + userName + '!');
-  alert('You are correct, ' + userName + '!')
-} else if (lowercaseDog === 'no' || lowercaseDog ==='n') {
-  // console.log('You must not know Rachel very well. Maybe you can redeem yourself on the next question.')
+  alert('You are correct, ' + userName + '!');
+} else if (lowercaseDog === 'no' || lowercaseDog === 'n') {
+  // console.log('You must not know Rachel very well. Maybe you can redeem yourself on the next question.');
   alert('You must not know Rachel very well. Maybe you can redeem yourself on the next question.');
 } else {
   // console.log('Did you understand the question?');
@@ -40,7 +40,7 @@ if (lowercaseCat === 'no' || lowercaseCat === 'n') {
   // console.log('That would be incorrect. Luna might eat the cat because she has a high prey drive.');
   alert('That would be incorrect. Luna might eat the cat because she has a high prey drive.');
 } else {
-  // console.log('I don\'t think you understood the question or possibly the instructions.')
+  // console.log('I don\'t think you understood the question or possibly the instructions.');
   alert('I don\'t think you understood the question or possibly the instructions.');
 }
 
@@ -56,7 +56,7 @@ if (lowercaseChildren === 'yes' || lowercaseChildren === 'y') {
   alert('Good job, ' + userName + '! Rachel has 3 children. She has 2 daughters and 1 son.');
 } else if(lowercaseChildren === 'no' || lowercaseChildren === 'n') {
   // console.log('That would be incorrect. Rachel has 3 children. She has 2 daughters and 1 son.');
-  alert('That would be incorrect. Rachel has 3 children. She has 2 daughters and 1 son.')
+  alert('That would be incorrect. Rachel has 3 children. She has 2 daughters and 1 son.');
 } else {
   // console.log('I don\'t think you are paying attention here.');
   alert('I don\'t think you are paying attention here.');
@@ -89,10 +89,10 @@ let lowercaseCity = cityAnswer.toLowerCase();
 // determines acceptable answers and returns an alert to whether the user was right or wrong
 if (lowercaseCity === 'no' || lowercaseCity === 'n') {
   // console.log('You are correct! Rachel is originally from Memphis, TN. The \"Home of the Blues, Elvis, and the World Championship BBQ cooking contest!\"');
-  alert('You are correct! Rachel is originally from Memphis, TN. The \"Home of the Blues, Elvis, and the World Championship BBQ cooking contest!\"');
+  alert('You are correct! Rachel is originally from Memphis, TN. The "Home of the Blues, Elvis, and the World Championship BBQ cooking contest!"');
 } else if (lowercaseCity === 'yes' || lowercaseCity === 'y') {
   // console.log('Womp! Womp! That would be incorrect! Rachel is from Memphis, TN. The \"Home of the Blues, Elvis, and the World Championship BBQ cooking contest!\"');
-  alert('Womp! Womp! That would be incorrect! Rachel is from Memphis, TN. The \"Home of the Blues, Elvis, and the World Championship BBQ cooking contest!\"');
+  alert('Womp! Womp! That would be incorrect! Rachel is from Memphis, TN. The "Home of the Blues, Elvis, and the World Championship BBQ cooking contest!"');
 } else {
   // console.log('I don\'t think you were even trying!');
   alert('I don\'t think you were even trying!');
