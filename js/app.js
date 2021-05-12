9f322f8676d1000e7f041f6eae6ded99e3848d9b
Rachel Freeland <rachel.freeland2@gmail.com>
2021-05-12 05:35:35 -0500

    CSS styling adjustment and typos on index.html fixed

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ let score = 0;
 function greeting() {
   // ask user their name so they can receive a custom greeting
   let userName = prompt('Hi user! What is your name?');
-  // console.log('Greetings ' + userName +'! Welcome to my page!');
   alert('Greetings ' + userName +'! Welcome to my page!');
   return userName;
 }
@@ -20,15 +19,12 @@ function dogQuestion() {
   let lowercaseDog = dogAnswer.toLowerCase();
   // determines acceptable answers and returns an alert to whether the user was right or wrong
   if (lowercaseDog === 'yes' || lowercaseDog === 'y') {
-    // console.log('You are correct, ' + userName + '!');
     alert('You are correct, ' + userName + '!');
     score++;
     console.log(score);
   } else if (lowercaseDog === 'no' || lowercaseDog === 'n') {
-    // console.log('You must not know Rachel very well. Maybe you can redeem yourself on the next question.');
     alert('You must not know Rachel very well. Maybe you can redeem yourself on the next question.');
   } else {
-    // console.log('Did you understand the question?');
     alert('Did you understand the question?');
   }
 }
@@ -42,14 +38,11 @@ function catQuestion() {
 
   // determines acceptable answers and returns an alert to whether the user was right or wrong
   if (lowercaseCat === 'no' || lowercaseCat === 'n') {
-    // console.log('That would be correct! Luna\'s prey drive is too high for her to live with a cat.');
     alert('That would be correct! Luna\'s prey drive is too high for her to live with a cat.');
     score++;
   } else if (lowercaseCat === 'yes' || lowercaseCat === 'y') {
-  // console.log('That would be incorrect. Luna might eat the cat because she has a high prey drive.');
     alert('That would be incorrect. Luna might eat the cat because she has a high prey drive.');
   } else {
-    // console.log('I don\'t think you understood the question or possibly the instructions.');
     alert('I don\'t think you understood the question or possibly the instructions.');
   }
 }
@@ -63,14 +56,11 @@ function children() {
 
   // determines acceptable answers and returns an alert to whether the user was right or wrong
   if (lowercaseChildren === 'yes' || lowercaseChildren === 'y') {
-    // console.log('Good job, ' + userName + '! Rachel has 3 children. She has 2 daughters and 1 son.');
     alert('Good job, ' + userName + '! Rachel has 3 children. She has 2 daughters and 1 son.');
     score++;
   } else if(lowercaseChildren === 'no' || lowercaseChildren === 'n') {
-    // console.log('That would be incorrect. Rachel has 3 children. She has 2 daughters and 1 son.');
     alert('That would be incorrect. Rachel has 3 children. She has 2 daughters and 1 son.');
   } else {
-    // console.log('I don\'t think you are paying attention here.');
     alert('I don\'t think you are paying attention here.');
   }
 }
@@ -84,14 +74,11 @@ function sibling() {
 
   // determines acceptable answers and returns an alert to whether the user was right or wrong
   if (lowercaseSibling === 'yes' || lowercaseSibling === 'y') {
-    // console.log('That is correct! Rachel has a sister.');
     alert('That is correct! Rachel has one sister.');
     score++;
   } else if (lowercaseSibling === 'no' || lowercaseSibling === 'n') {
-    // console.log('Ooops! The correct answer would be \'yes\'. Rachel has a younger sister.');
     alert('Ooops! The correct answer would be \'yes\'. Rachel has a younger sister.');
   } else {
-    // console.log('You should understand how this works by now. I don\'t think you are trying.');
     alert('You should understand how this works by now. I don\'t think you are trying.');
   }
 }
@@ -103,14 +90,11 @@ function homeTown() {
   let lowercaseCity = cityAnswer.toLowerCase();
   // determines acceptable answers and returns an alert to whether the user was right or wrong
   if (lowercaseCity === 'no' || lowercaseCity === 'n') {
-    // console.log('You are correct! Rachel is originally from Memphis, TN. The \"Home of the Blues, Elvis, and the World Championship BBQ cooking contest!\"');
     alert('You are correct! Rachel is originally from Memphis, TN. The "Home of the Blues, Elvis, and the World Championship BBQ cooking contest!"');
     score++;
   } else if (lowercaseCity === 'yes' || lowercaseCity === 'y') {
-    // console.log('Womp! Womp! That would be incorrect! Rachel is from Memphis, TN. The \"Home of the Blues, Elvis, and the World Championship BBQ cooking contest!\"');
     alert('Womp! Womp! That would be incorrect! Rachel is from Memphis, TN. The "Home of the Blues, Elvis, and the World Championship BBQ cooking contest!"');
   } else {
-    // console.log('I don\'t think you were even trying!');
     alert('I don\'t think you were even trying!');
   }
 }
